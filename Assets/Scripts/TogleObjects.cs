4e68eef21b6cc95b7304ea557f57222de4b44ad3
LeJose <jdyepes@hotmail.com>
2021-03-28 13:57:37 -0500

    fOOD IMAGES DISPLAY

---
+++ Assets/Scripts/TogleObjects.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TogleObjects : MonoBehaviour
+{
+    [SerializeField] private GameObject[] objs;
+    [SerializeField] private GameObject togleOnButton;
+    [SerializeField] private GameObject togleOffButton;
+
+    public void Togle(bool on)
+    {
+        for (int i = 0; i < objs.Length; i++)
+        {
+            objs[i].SetActive(on);
+        }
+
+        if (on)
+        {
+            togleOffButton.SetActive(true);
+            togleOnButton.SetActive(false);
+        }
+        else
+        {
+            togleOffButton.SetActive(false);
+            togleOnButton.SetActive(true);
+        }
+    }
+}
